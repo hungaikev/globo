17100bc0176487d9a0135698bc3b1a422ba9022b
Hungai Amuhinda <hungaiinc@gmail.com>
2020-03-24 08:15:25 +0300

    ELB

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ provider "aws" {
   region  = var.aws_region
 }
 
+data "aws_availability_zones" "available" {}
+
 data "aws_ami" "aws_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -21,28 +23,94 @@ data "aws_ami" "aws_linux" {
   }
 }
 
-# This uses the default VPC. It will NOT delete it on destroy
-resource "aws_default_vpc" "default" {
-
+resource "aws_vpc" "vpc" {
+  cidr_block = var.network_address_space
+  enable_dns_hostnames = "true"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = var.subnet1_address_space
+  vpc_id = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = var.subent2_address_space
+  vpc_id = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone = data.aws_availability_zones.available.names[1]
+}
+
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  route_table_id = aws_route_table.rtb.id
+  subnet_id = aws_subnet.subnet1.id
+}
+
+resource "aws_route_table_association" "rta-subnet2" {
+  route_table_id = aws_route_table.rtb.id
+  subnet_id = aws_subnet.subnet2.id
+}
+
+# SECURITY GROUPS
+# ELB Security Group
+resource "aws_security_group" "elb-sg" {
+  name = "nginx_elb_sg"
+  description = "Allow HTTP from anywhere"
+  vpc_id = aws_vpc.vpc.id
+
+  #Allow inbound
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # allow all outbound
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+# Nginx Security group
 resource "aws_security_group" "allow-ssh" {
   name        = "nginx-demo"
   description = "Allow ports for nginx demo"
-  vpc_id      = aws_default_vpc.default.id
+  vpc_id      = aws_vpc.vpc.id
 
+  # SSH access from anywhere
   ingress {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # HTTP access from anywhere
   ingress {
     from_port   = 80
     protocol    = "tcp"
     to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.network_address_space]
   }
+  # Outbound internet access
+
   egress {
     from_port   = 0
     protocol    = -1
@@ -51,9 +119,25 @@ resource "aws_security_group" "allow-ssh" {
   }
 }
 
-resource "aws_instance" "nginx" {
+# Load balancer
+resource "aws_elb" "web" {
+  name = "nginx-elb"
+
+  subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  security_groups = [aws_security_group.elb-sg.id]
+  instances = [aws_instance.nginx1.id, aws_instance.nginx2.id]
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+}
+
+resource "aws_instance" "nginx1" {
   ami                    = data.aws_ami.aws_linux.id
   instance_type          = "t2.micro"
+  subnet_id = aws_subnet.subnet1.id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
@@ -67,11 +151,36 @@ resource "aws_instance" "nginx" {
   provisioner "remote-exec" {
     inline = [
       "sudo yum install nginx -y",
-      "sudo service nginx start"
+      "sudo service nginx start",
+      "echo '<html><head><title>Blue Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
     ]
   }
 }
 
+resource "aws_instance" "nginx2" {
+  ami                    = data.aws_ami.aws_linux.id
+  instance_type          = "t2.micro"
+  subnet_id = aws_subnet.subnet2.id
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start",
+      "echo '<html><head><title>Green Team Server</title></head><body style=\"background-color:#77A032\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
+    ]
+  }
+}
+
+
 output "aws_public_ip" {
-  value = aws_instance.nginx.public_dns
+  value = aws_elb.web.dns_name
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,15 @@ variable "private_key_path" {}
 variable "key_name" {
   default = "kafka_cluster"
 }
+
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+
+variable "subent2_address_space" {
+  default = "10.1.1.0/24"
+}
